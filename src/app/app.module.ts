b17c1d17b88d6a3b1a50026e4199be549524bf8c
Mohammed Rizwan <MohammedRizwan6477@gmail.com>
2022-08-09 19:00:59 +0530

    html and css code

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FormsModule,ReactiveFormsModule} from "@angular/forms";
+import { Task1Component } from './task1/task1.component';
+import { Task2Component } from './task2/task2.component';
+import { Task3Component } from './task3/task3.component'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Task1Component,
+    Task2Component,
+    Task3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
